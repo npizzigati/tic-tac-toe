6683680ef0596da4736b54ffb4ba15328c931d17
Nicholas Pizzigati <npizzigati@gmail.com>
2020-05-03 16:25:59 -0500

    test commit

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -1,9 +1,9 @@
 require 'pry'
 
-
 require_relative 'minimax.rb'
 require_relative 'display.rb'
 
+ 
 class Board
   WINNING_LINES = [0, 1, 2], [3, 4, 5], [6, 7, 8], # horizontal
                    [0, 3, 6], [1, 4, 7], [2, 5, 8], # vertical
